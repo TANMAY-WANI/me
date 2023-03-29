94422782560e73e1575654bb9e76952454c58a7b
Tanmay Wani <97271195+TANMAY-WANI@users.noreply.github.com>
2023-03-29 15:44:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,82 @@
+
+
+$(".yellow").mouseover(function(){
+    $(".yellow").addClass("hover");
+    $(".yellow").addClass("pointer")
+})
+
+$(".yellow").mouseout(function(){
+    $(".yellow").removeClass("hover");
+    $(".yellow").removeClass("pointer")
+})
+
+$(".yellow").click(function(){
+    window.location.href = "resume.html"
+})
+
+$(".red").mouseover(function(){
+    $(".red").addClass("hover");
+    $(".red").addClass("pointer")
+})
+
+$(".red").mouseout(function(){
+    $(".red").removeClass("hover");
+    $(".red").removeClass("pointer")
+})
+
+$(".red").click(function(){
+    window.location.href = "projects.html"
+})
+
+$(".blue").mouseover(function(){
+    $(".blue").addClass("hover");
+    $(".blue").addClass("pointer")
+})
+
+$(".blue").mouseout(function(){
+    $(".blue").removeClass("hover");
+    $(".blue").removeClass("pointer")
+})
+$(".blue").click(function(){
+    window.location.href = "contact.html"
+})
+
+$(".insta").mouseover(function(){
+    $(".insta").addClass("pointer");
+})
+$(".insta").mouseout(function(){
+    $(".insta").removeClass("pointer");
+})
+$(".insta").click(function(){
+    window.location.href = "https://www.instagram.com/tanmay_2_6/";
+})
+
+$(".twitter").mouseover(function(){
+    $(".twitter").addClass("pointer");
+})
+$(".twitter").mouseout(function(){
+    $(".twitter").removeClass("pointer");
+})
+$(".twitter").click(function(){
+    window.location.href = "https://twitter.com/TanmayWani17?t=xnBL3LpWO2V0ZdffnxLplw&s=09";
+})
+
+$(".linkedIn").mouseover(function(){
+    $(".linkedIn").addClass("pointer");
+})
+$(".linkedIn").mouseout(function(){
+    $(".linkedIn").removeClass("pointer");
+})
+$(".linkedIn").click(function(){
+    window.location.href = "https://www.linkedin.com/in/tanmay-wani-143050216";
+})
+
+$(".myName").mouseover(function(){
+    $(".myName").addClass("pointer");
+})
+$(".myName").mouseout(function(){
+    $(".myName").removeClass("pointer");
+})
+$(".myName").click(function(){
+    window.location.href = "index.html"
+})
---
+++ resume.js
@@ -0,0 +1,38 @@
+$(".insta").mouseover(function(){
+    $(".insta").addClass("pointer");
+})
+$(".insta").mouseout(function(){
+    $(".insta").removeClass("pointer");
+})
+$(".insta").click(function(){
+    window.location.href = "https://www.instagram.com/tanmay_2_6/";
+})
+
+$(".twitter").mouseover(function(){
+    $(".twitter").addClass("pointer");
+})
+$(".twitter").mouseout(function(){
+    $(".twitter").removeClass("pointer");
+})
+$(".twitter").click(function(){
+    window.location.href = "https://twitter.com/TanmayWani17?t=xnBL3LpWO2V0ZdffnxLplw&s=09";
+})
+
+$(".linkedIn").mouseover(function(){
+    $(".linkedIn").addClass("pointer");
+})
+$(".linkedIn").mouseout(function(){
+    $(".linkedIn").removeClass("pointer");
+})
+$(".linkedIn").click(function(){
+    window.location.href = "https://www.linkedin.com/in/tanmay-wani-143050216";
+})
+$(".myName").click(function(){
+    window.location.href = "index.html"
+})
+$(".myName").mouseover(function(){
+    $(".myName").addClass("pointer");
+})
+$(".myName").mouseout(function(){
+    $(".myName").removeClass("pointer");
+})
